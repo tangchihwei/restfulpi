5de6db1c49b185cc06362716e6e62a8f020425d5
Chih-Wei Tang <tang.chihwei@gmail.com>
2017-05-12 11:46:04 -0700

    added json object

--- restfulpi.py
+++ restfulpi.py
@@ -1,10 +1,12 @@
 import requests
 import json
 
-backend = 'http://0.0.0.0:5000/scan'
+backend = 'https://gfl-turnstile.herokuapp.com/scan'
 headers = {'Content-type': 'application/json'}
 data = {"id":"12667"} 
 
-
 resp = requests.post(backend, json.dumps(data), headers = headers)
-resp.json
+
+# parse into json object
+respJson = json.loads(resp.text)
+print respJson
