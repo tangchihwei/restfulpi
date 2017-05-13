1a0fd1d2248e6d3989520a2cd1ecaca72fd12562
Chih-Wei Tang <tang.chihwei@gmail.com>
2017-05-13 15:32:42 -0700

    updated api call with get

--- restfulpi.py
+++ restfulpi.py
@@ -1,12 +1,20 @@
 import requests
 import json
 
-backend = 'https://gfl-turnstile.herokuapp.com/scan'
+backend = 'localhost'
 headers = {'Content-type': 'application/json'}
-data = {"id":"12667"} 
+cardid = {"id":"CLIPPER_CARD_001"} 
 
-resp = requests.post(backend, json.dumps(data), headers = headers)
+# resp = requests.post(backend+"scan", json.dumps(cardid), headers = headers)
+resp = requests.get(backend+"user/"+str(cardid['id']))
 
 # parse into json object
 respJson = json.loads(resp.text)
-print respJson
+print "User Name: "+respJson['who']
+print "Account Balance: $"+str(respJson['balance'])
+print "Last Trip: $"+str(respJson['lastTrip'])
+# print "Transit Agency: "+respJson['agency']
+# print "Trip Origin: "+respJson['origin']
+# print "Trip Destination: "+respJson['destination']
+# print "Trip Referral: "+respJson['referral']
+
