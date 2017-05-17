f283899b5bf2a433a75127913fe1f2c496369db5
Chih-Wei Tang <tang.chihwei@gmail.com>
2017-05-16 22:38:28 -0700

    added in class

--- restfulpi.py
+++ restfulpi.py
@@ -1,21 +1,31 @@
 import requests
 import json
 
-backend = 'http://localhost:5000/'
-headers = {'Content-type': 'application/json'}
-cardid = {"id":"CLIPPER_CARD_001"} 
+# cardid = {"id":"CLIPPER_CARD_001"} 
 
 # resp = requests.post(backend+"scan", json.dumps(cardid), headers = headers)
-resp = requests.get(backend+"user/"+str(cardid['id']))
-
-# parse into json object
-respJson = json.loads(resp.text)
-print "User Name: "+respJson['who']
-print "Account Balance: $"+str(respJson['balance'])
-print "Last Trip: $"+str(respJson['lastTrip'])
 
 data={ "id":"CLIPPERCARD_001", "agency":"BART",	"origin":"MONTGOMERY","destination":"MILLBRAE", "referral":"GOOGLE"}
 
-resp = requests.post(backend+"ticket", json.dumps(data), headers = headers)
-respJson = json.loads(resp.text)
-print(str(respJson))
+# resp = requests.post(backend+"ticket", json.dumps(data), headers = headers)
+# respJson = json.loads(resp.text)
+# print(str(respJson))
+
+class GflGate():
+	def __init__(self):
+		self._backend = 'http://localhost:5000/'
+		self._headers = {'Content-type': 'application/json'}
+
+	def hello(self):
+		return "hello GFL"
+
+	def tap(self, uid):
+		tapResp = json.loads(requests.get(self._backend + "user/"+str(uid)).text)
+		return tapResp #return dictionary form
+
+if __name__ == "__main__":
+	gate = GflGate()
+	print(gate.hello())
+	resp = gate.tap("CLIPPER_CARD_001")
+	print "User Name: "+resp['who']
+
