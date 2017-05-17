9810a982b6f4e2fc04877969269472b2be826faf
Chih-Wei Tang <tang.chihwei@gmail.com>
2017-05-16 22:52:06 -0700

    added run method

--- restfulpi.py
+++ restfulpi.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 # cardid = {"id":"CLIPPER_CARD_001"} 
 
@@ -16,16 +17,29 @@ class GflGate():
 		self._backend = 'http://localhost:5000/'
 		self._headers = {'Content-type': 'application/json'}
 
+
 	def hello(self):
 		return "hello GFL"
 
-	def tap(self, uid):
+	def tap_request(self, uid):
 		tapResp = json.loads(requests.get(self._backend + "user/"+str(uid)).text)
 		return tapResp #return dictionary form
+
+	def run(self):
+		_card_present = False
+
+		while True:
+			
+			time.sleep(1)
+
+
+
+
 
 if __name__ == "__main__":
 	gate = GflGate()
 	print(gate.hello())
-	resp = gate.tap("CLIPPER_CARD_001")
+	resp = gate.tap_request("CLIPPER_CARD_001")
 	print "User Name: "+resp['who']
+	gate.run()
 
