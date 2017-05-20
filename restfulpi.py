57032d65606a4cdfa386da11918b5b191bf1e87a
Chih-Wei Tang <tang.chihwei@gmail.com>
2017-05-19 21:49:30 -0700

    added ts command

--- restfulpi.py
+++ restfulpi.py
@@ -1,24 +1,44 @@
 import requests
 import json
 import time
+import Adafruit_BBIO.UART as UART
+import serial
+
+_cmd_left_on = bytearray([0x7E, 0x80, 0x00, 0x01, 0x00, 0x00, 0x80, 0xAA, 0x00, 0x01, 0x01, 0x00, 0xDE, 0x62, 0x7E])
+_cmd_right_on = bytearray([0x7E, 0x80, 0x00, 0x01, 0x00, 0x00, 0x80, 0xAA, 0x00, 0x01, 0x02, 0x00, 0x8B, 0x31, 0x7E])
+
 
 class GflGate():
 	def __init__(self):
 		self._backend = 'http://localhost:5000/'
 		self._headers = {'Content-type': 'application/json'}
-
+		UART.setup("UART1")
+		ser = serial.Serial(port = "/dev/ttyS1", baudrate = 19200)
+		ser.close()
+		ser.open()
 
 	def hello(self):
-		return "hello GFL"
+		return "Hello GFL"
 
 	def tap_request(self, card_id):
-		tapResp = json.loads(requests.get(self._backend + "user/"+str(card_id)).text)
-		return tapResp #return dictionary form
+		tap_resp = json.loads(requests.get(self._backend + "user/"+str(card_id)).text)
+		return tap_resp #return dictionary form
+
+	def _turn_on_left_gate():
+		print "turn on left gate"
+		ser.write(_cmd_left_on)
+
+	def _turn_on_right_gate():
+		print "turn on right gate"
+		ser.write(_cmd_right_on)
 
 	def read_card(self):
 		return "CLIPPER_CARD_001"
+		#TODO: read card nfc interface
 
-	# def gate_control():
+	def gate_control(self, direction):
+		# TODO: call gate control 
+		print "Opening Gate" + str(direction)
 
 	def run(self):
 		_card_present = False
